1fbe361142605450f84393d77ab890d06b579f17
Gusty Blanco <geblancoknippel@gmail.com>
2023-02-08 21:08:53 -0300

    Punto seguro, models listos

---
+++ AppCoder/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+def curso(self):
+    curso = Curso(nombre="Desarrollo Web", comision=19881)
+    curso.save()
+    documentoDeTexto = f"--> Curso: {curso.nombre} comision: {curso.comision}"
+    return HttpResponse(documentoDeTexto)
+
+def estudiante(self):
+    curso = Curso(nombre="Desarrollo Web", comision=19881)
+    curso.save()
+    documentoDeTexto = f"--> Curso: {curso.nombre} comision: {curso.comision}"
+    return HttpResponse(documentoDeTexto)
